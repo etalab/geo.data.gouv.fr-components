db1e5104a39b4086046423ae3e9c9a98e9b33403
tmerlier <tmerlier@student.42.fr>
2018-03-06 10:13:04 +0100

    Improvement of LeafletMap component

--- components/leaflet-map.js
+++ components/leaflet-map.js
@@ -10,11 +10,17 @@ L.Icon.Default.imagePath = '/static/images/leaflet/'
 
 class LeafletMap extends React.Component {
   render() {
-    const {center, position, zoom, data} = this.props
+    const {center, viewport, position, zoom, data, fullscreen} = this.props
+    let bounds
+
+    if (data) {
+      const poly = L.polygon(data.coordinates[0].map(coord => [coord[1], coord[0]]))
+      bounds = poly.getBounds()
+    }
 
     return (
-      <div>
-        <Map center={center} zoom={zoom} scrollWheelZoom={false}>
+      <div className={fullscreen ? `fullscreen` : 'window'}>
+        <Map viewport={viewport} center={center} zoom={zoom} bounds={bounds} scrollWheelZoom={false}>
           <TileLayer
             url='https://tilecache.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'
             attribution='Map data &copy; 2012 OpenStreetMap contributors' />
@@ -22,6 +28,7 @@ class LeafletMap extends React.Component {
           {position && <Marker position={position} />}
 
           {data && <GeoJSON
+            key={data.coordinates[0][0][0]}
             color='blue'
             fillOpacity={0.1}
             weight={2}
@@ -31,13 +38,41 @@ class LeafletMap extends React.Component {
         <style dangerouslySetInnerHTML={{__html: mapStyle}} />
 
         <style jsx>{`
-          div {
+          .fullscreen {
+            width: 100%;
+            position: fixed;
+            top: 74px;
+            bottom: 0;
+            left: 0;
+            right: 0;
+          }
+
+          .fullscreen :global(.leaflet-container) {
+            height: 100%;
+          }
+
+          .fullscreen :global(.photon-input) {
+            width: 400px;
+          }
+
+          .fullscreen :global(.leaflet-right) {
+            right: calc(50% - 200px);
+          }
+
+          @media (max-width: 550px) {
+            .fullscreen {
+              width: 100%;
+              top: 164px;
+            }
+          }
+
+          .window {
             width: 100%;
             max-width: 500px;
             height: 400px;
           }
 
-          div :global(.leaflet-container) {
+          .window :global(.leaflet-container) {
             height: 100%;
           }
         `}</style>
@@ -49,15 +84,25 @@ class LeafletMap extends React.Component {
 LeafletMap.propTypes = {
   data: PropTypes.object,
   center: PropTypes.array,
+  viewport: PropTypes.shape({
+    center: PropTypes.array,
+    zoom: PropTypes.number
+  }),
   position: PropTypes.array,
-  zoom: PropTypes.number
+  zoom: PropTypes.number,
+  fullscreen: PropTypes.bool
 }
 
 LeafletMap.defaultProps = {
   data: null,
   position: null,
+  viewport: {
+    center: [46.921982, 2.978952],
+    zoom: 5
+  },
   center: [46.921982, 2.978952],
-  zoom: 5
+  zoom: 5,
+  fullscreen: false
 }
 
 export default LeafletMap
